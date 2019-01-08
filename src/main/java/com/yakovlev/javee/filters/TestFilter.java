13bc81b0e40c4e8d2333dfd4f19dc2d3c8222a35
yakovlev <ay@skb.ru>
2019-01-08 22:39:17 +0500

    home work 2

---
+++ src/main/java/com/yakovlev/javee/filters/TestFilter.java
@@ -0,0 +1,29 @@
+package com.yakovlev.javee.filters;
+
+import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/*
+@author Yakovlev
+*/
+
+@WebFilter ("/*")
+public class TestFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html;charset=utf-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/yakovlev/javee/pages/ProductPageServlet.java
@@ -0,0 +1,25 @@
+package com.yakovlev.javee.pages;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/*
+@author Yakovlev
+*/
+@WebServlet("/product")
+public class ProductPageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try(PrintWriter out = resp.getWriter()){
+            out.println(Teamplate.HEADER_BEFORE_TITLE+"Product"+Teamplate.HEADER_AFTER_TITLE);
+            out.println("<h1>- Product page -</h1>");
+            out.println(Teamplate.MENUE);
+            out.println(Teamplate.FOOTER);
+        }
+    }
+}
